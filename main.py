ef91e92935816c85de35b6b2e00cf59f1b395304
Grzegorz Strelczuk <maxtmus@live.com>
2018-05-21 00:14:04 +0200

    ...

--- main.py
+++ main.py
@@ -4,44 +4,44 @@ import sys
 from tkinter import *
 
 
-window_width = 1200 #in pixels
-window_height = 1200 #in pixels
-map_width = 200 #number of cells
-map_height = 200 #number of cells
+window_width = 1200  # in pixels
+window_height = 1200  # in pixels
+map_width = 200   # number of cells
+map_height = 200  # number of cells
 scale = 5
 
 # ---- CLASSES ----
 
 class Material:
-    index = 0 #number of created materials
+    index = 0  # number of created materials
 
     def __init__(self, name, fuel, auto_ignition_temp, flash_point_temp, state, burning_temp, color):
         Material.index = Material.index + 1
         self.name = name
-        self.fuel = fuel #hit points, minutes of burning
-        self.auto_ignition_temp = auto_ignition_temp #in Celsius
+        self.fuel = fuel  # hit points, minutes of burning
+        self.auto_ignition_temp = auto_ignition_temp  # in Celsius
         self.flash_point_temp = flash_point_temp
         self.state = state
         self.id = Material.index
         self.color = color
         self.burning_temp = burning_temp
 
-        #state:
-        #0 cannot burn
-        #1 can burn
-        #2 burning
-        #3 burned
+        # state:
+        # 0 cannot burn
+        # 1 can burn
+        # 2 burning
+        # 3 burned
 
 class Cell:
 
     def __init__(self, material, humidity, temp, downfall, height):
         self.material = material
-        self.humidity = humidity #from 0 to 10
+        self.humidity = humidity # from 0 to 10
         self.temp = temp
         self.downfall = downfall
         self.height = height
 
-        #downfall
+        # downfall
         # <-10, -1> snow
         # 0 nothing
         # <1, 10> rain
@@ -52,8 +52,8 @@ class Map:
     def __init__(self, width, height, canvas):
         self.canvas = canvas
         self.grid = [[0 for x in range(width)] for y in range(height)]
-        self.wind_dir = randint(0, 359) #integer degree, cycling
-        self.wind_power = randint(0 , 12) #beaufort
+        self.wind_dir = randint(0, 359) # integer degree, cycling
+        self.wind_power = randint(0 , 12) # beaufort
 
     # ---- METODY ----
 
@@ -74,7 +74,7 @@ class Map:
                 else:                                                                   # Grass -> rest
                     self.grid[i][j] = Cell(copy.copy(materials[1]), 3, 22, 0, 17)
 
-        #Trees                                                                          # Trees overwrite
+        # Trees                                                                          # Trees overwriteK
                                                                                         # current cell with 25%
         for i in range(1, map_height - 1):
             for j in range(1, map_width - 1):
@@ -129,11 +129,11 @@ class Map:
         y_off = window_height/2 - map_height*scale/2
         for i in range(0, map_height):
             for j in range(0, map_width):
-                #col = "#%03x"%randint(0, 0xFFF)
+                # col = "#%03x"%randint(0, 0xFFF)
                 col = self.grid[i][j].material.color
                 self.canvas.create_rectangle(j*scale+x_off, i*scale+y_off, (j+1)*scale+x_off, (i+1)*scale+y_off, fill=col)
 
-        #canvas.create_rectangle(300, 300, 200, 10, fill="blue", outline="blue")
+        # canvas.create_rectangle(300, 300, 200, 10, fill="blue", outline="blue")
         self.canvas.pack()
         self.canvas.after(500, self.drawMap)
 
@@ -141,7 +141,7 @@ class Map:
         w_dir = randint(-1, 1)
         w_pow = randint(1, 12)
 
-        #if w_dir == 1 AND self.wind_dir == 359
+        # if w_dir == 1 AND self.wind_dir == 359
             #self.wind_dir
 
 
