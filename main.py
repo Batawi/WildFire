ed271c502ec73758f1acfa351a714169a8c475e4
Batawi <batawi@debian>
2018-05-20 17:02:14 +0200

    wind is comming

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ window_height = 600 #in pixels
 map_width = 50 #number of cells
 map_height = 50 #number of cells
 scale = 10
-time_stamp = 1000 #time_stamp[ms] = 1[min] of simulation
+time_stamp = 500 #time_stamp[ms] = 1[min] of simulation
 
 
 # ---- CLASSES ----
@@ -54,9 +54,9 @@ class Map:
 
     def __init__(self, width, height, canvas):
         self.canvas = canvas
-        self.grid = [[0 for x in range(width+2)] for y in range(height+2)]
-        self.wind_dir = randint(0, 359) #integer degree, cycling
-        self.wind_power = randint(0 , 12) #beaufort
+        self.grid = [[0 for x in range(width+6)] for y in range(height+6)]
+        self.wind_dir = randint(0, 7) #integer degree, cycling
+        self.wind_power = random.choice([0, 0, 0, 1, 1, 1, 2, 2, 3])
         self.texts_to_update = []
 
     # ---- METODY ----
@@ -75,22 +75,30 @@ class Map:
         self.canvas.after(time_stamp, self._updateTexts)
 
     def generateRandomMap(self, materials):
-        for i in range(1, map_height+1):
-            for j in range(1, map_width+1):
+        for i in range(3, map_height+3):
+            for j in range(3, map_width+3):
                 self.grid[i][j] = Cell(copy.copy(random.choice(materials)), randint(0, 10), randint(5, 35), 0, randint(0, 100))
 
     def randomCosmicGenerator(self, materials):
-        # water border
-        for i in range(0, map_height+2):
+        # water border, size = 3
+        for i in range(0, map_height+6):
             self.grid[i][0] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
-            self.grid[i][map_width+1] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
-        for i in range(0, map_width+2):
+            self.grid[i][1] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[i][2] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[i][map_width+3] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[i][map_width+4] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[i][map_width+5] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+        for i in range(3, map_width+3):
             self.grid[0][i] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
-            self.grid[map_height+1][i] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[1][i] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[2][i] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[map_height+3][i] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[map_height+4][i] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
+            self.grid[map_height+5][i] = Cell(copy.copy(materials[0]), 10, 12, 0, 12)
 
         # Grass/Bushes/logs
-        for i in range(1,map_height+1):
-            for j in range(1,map_width+1):
+        for i in range(3,map_height+3):
+            for j in range(3,map_width+3):
                 if randint(0, 8) == 3: # Log -> 12,5%
                     self.grid[i][j] = Cell(copy.copy(materials[2]), 3, 22, 0, 17)
                 elif randint(0, 2) == 1: # Bush -> 17,5%
@@ -100,8 +108,8 @@ class Map:
 
         #Trees  # Trees overwrite
                 # current cell with 25%
-        for i in range(2, map_height):
-            for j in range(2, map_width):
+        for i in range(4, map_height+2):
+            for j in range(4, map_width+2):
                 if randint(0, 3) == 1:
                     self.grid[i][j] = Cell(copy.copy(materials[4]), 3, 22, 0, 17)
 
@@ -146,10 +154,10 @@ class Map:
                     i -= 1
 
     def drawMap(self):
-        x_off = window_width/2 - map_width*scale/2
-        y_off = window_height/2 - map_height*scale/2
-        for i in range(0, map_height+2):
-            for j in range(0, map_width+2):
+        x_off = window_width/2 - (map_width+6)*scale/2
+        y_off = window_height/2 - (map_height+6)*scale/2
+        for i in range(0, map_height+6):
+            for j in range(0, map_width+6):
                 #col = "#%03x"%randint(0, 0xFFF)
                 col = self.grid[i][j].material.color
                 self.canvas.create_rectangle(j*scale+x_off, i*scale+y_off, (j+1)*scale+x_off, (i+1)*scale+y_off, fill=col)
@@ -158,21 +166,21 @@ class Map:
         self.canvas.after(time_stamp, self.drawMap)
 
     def setWind(self):
-        w_dir = random.choice([-1, 0, 1])
+        w_dir = random.choice([-1, 0, 0, 0, 0, 0, 1])
         #w_pow = random.choice([-1, 0, 0, 0, 0, 0, 0, 0, 1]) #sila wiatru bedzie sie zmieniac wolniej
         w_pow = randint(-20, 20)
 
-        if w_dir == 1 and self.wind_dir == 359:
+        if w_dir == 1 and self.wind_dir == 7:
             self.wind_dir = 0
-        elif  w_dir == -1 and self.wind_dir == 0:
-            self.wind_dir = 359
+        elif w_dir == -1 and self.wind_dir == 0:
+            self.wind_dir = 7
         else:
             self.wind_dir += w_dir
 
-        if w_pow == 1 and self.wind_power == 12:
-            self.wind_power = 12
-        elif w_pow == -1 and self.wind_power == 1:
-            self.wind_power = 1
+        if w_pow == 1 and self.wind_power == 3:
+            self.wind_power = 3
+        elif w_pow == -1 and self.wind_power == 0:
+            self.wind_power = 0
         elif w_pow == 1:
             self.wind_power += 1
         elif w_pow == -1:
@@ -193,8 +201,8 @@ class Map:
 
     def simulation(self):
         print("symulacja")
-        ignition_update = [[0 for x in range(map_width+2)] for y in range(map_height+2)] #ignite new cells
-        burn_update = [[0 for x in range(map_width+2)] for y in range(map_height+2)] #Hp management
+        ignition_update = [[0 for x in range(map_width+6)] for y in range(map_height+6)] #ignite new cells
+        burn_update = [[0 for x in range(map_width+6)] for y in range(map_height+6)] #Hp management
 
         for i in range(1, map_height+1):
             for j in range(1, map_width+1):
@@ -203,33 +211,79 @@ class Map:
                     # --- FLASH POINT IGNITION ---
                     temp = self.grid[i][j].material.burning_temp #temp of current cell
 
+                    if(self.wind_power == 0 or self.wind_power == 1):
+
+                        if(self.grid[i-1][j].material.state == 1 and
+                        self.grid[i-1][j].material.flash_point_temp <= temp):
+                        ignition_update[i-1][j] = 1
+
+                        if(self.grid[i][j-1].material.state == 1 and
+                        self.grid[i][j-1].material.flash_point_temp <= temp):
+                        ignition_update[i][j-1] = 1
+
+                        if(self.grid[i][j+1].material.state == 1 and
+                        self.grid[i][j+1].material.flash_point_temp <= temp):
+                        ignition_update[i][j+1] = 1
+
+                        if(self.grid[i+1][j].material.state == 1 and
+                        self.grid[i+1][j].material.flash_point_temp <= temp):
+                        ignition_update[i+1][j] = 1
+
+                        # power 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 0): #N
+                            if(self.grid[i-2][j].material.state == 1 and
+                            self.grid[i-2][j].material.flash_point_temp <= temp):
+                            ignition_update[i-2][j] = 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 1): #NE
+                            if(self.grid[i+1][j+1].material.state == 1 and
+                            self.grid[i+1][j+1].material.flash_point_temp <= temp):
+                            ignition_update[i+1][j+1] = 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 2): #E
+                            if(self.grid[i][j+2].material.state == 1 and
+                            self.grid[i][j+2].material.flash_point_temp <= temp):
+                            ignition_update[i][j+2] = 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 3): #ES
+                            if(self.grid[i+1][j+1].material.state == 1 and
+                            self.grid[i+1][j+1].material.flash_point_temp <= temp):
+                            ignition_update[i+1][j+1] = 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 4): #S
+                            if(self.grid[i+2][j].material.state == 1 and
+                            self.grid[i+2][j].material.flash_point_temp <= temp):
+                            ignition_update[i+2][j] = 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 5): #SW
+                            if(self.grid[i+1][j-1].material.state == 1 and
+                            self.grid[i+1][j-1].material.flash_point_temp <= temp):
+                            ignition_update[i+1][j-1] = 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 6): #W
+                            if(self.grid[i][j-2].material.state == 1 and
+                            self.grid[i][j-2].material.flash_point_temp <= temp):
+                            ignition_update[i][j-2] = 1
+
+                        if(self.wind_power == 1 and self.wind_dir == 7): #WN
+                            if(self.grid[i-1][j].material.state == 1 and
+                            self.grid[i-1][j].material.flash_point_temp <= temp):
+                            ignition_update[i-1][j] = 1
+
+
+
                     if(self.grid[i-1][j-1].material.state == 1 and
                     self.grid[i-1][j-1].material.flash_point_temp <= temp):
                         ignition_update[i-1][j-1] = 1
-
-                    if(self.grid[i-1][j].material.state == 1 and
-                    self.grid[i-1][j].material.flash_point_temp <= temp):
-                        ignition_update[i-1][j] = 1
 
                     if(self.grid[i-1][j+1].material.state == 1 and
                     self.grid[i-1][j+1].material.flash_point_temp <= temp):
                         ignition_update[i-1][j+1] = 1
 
-                    if(self.grid[i][j-1].material.state == 1 and
-                    self.grid[i][j-1].material.flash_point_temp <= temp):
-                        ignition_update[i][j-1] = 1
-
-                    if(self.grid[i][j+1].material.state == 1 and
-                    self.grid[i][j+1].material.flash_point_temp <= temp):
-                        ignition_update[i][j+1] = 1
-
                     if(self.grid[i+1][j-1].material.state == 1 and
                     self.grid[i+1][j-1].material.flash_point_temp <= temp):
                         ignition_update[i+1][j-1] = 1
-
-                    if(self.grid[i+1][j].material.state == 1 and
-                    self.grid[i+1][j].material.flash_point_temp <= temp):
-                        ignition_update[i+1][j] = 1
 
                     if(self.grid[i+1][j+1].material.state == 1 and
                     self.grid[i+1][j+1].material.flash_point_temp <= temp):
@@ -240,8 +294,8 @@ class Map:
 
 
 
-        for i in range(1, map_height+1):
-            for j in range(1, map_width+1):
+        for i in range(3, map_height+3):
+            for j in range(3, map_width+3):
                 if(ignition_update[i][j] == 1):
                     self.grid[i][j].material.state = 2
                     self.grid[i][j].material.color = "#ff0000"
@@ -289,7 +343,7 @@ area.ignition()
 area.simulation()
 
 area.drawMap()
-#area.setWind()
+area.setWind()
 area.showClassVariables()
 
 #loop start
